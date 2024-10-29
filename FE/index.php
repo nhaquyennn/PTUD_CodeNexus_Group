46218a54c7a1177113656d8da09402e368c1b0a4
GiaHi1 <giahi0000@gmail.com>
2024-10-29 13:58:52 +0700

    Gia Hi card

--- FE/index.php
+++ FE/index.php
@@ -2,7 +2,8 @@
 <html lang="zxx">
 
 <head>
-    <?php require_once 'layout/header.php'?>
+    <?php require_once 'layout/header.php' ?>
+    <?php require_once 'layout/header_section.php' ?>
 </head>
 
 <body>
@@ -11,119 +12,6 @@
         <div class="loader"></div>
     </div>
 
-    <!-- Header Section Begin -->
-    <header class="header">
-        <div class="header__top">
-            <div class="container">
-                <div class="row">
-                    <div class="col-lg-8 col-md-8">
-                        <div class="header__top__left">
-                            <ul>
-                                <li><i class="fa fa-envelope"></i>codenexus@gmail.com</li>
-                                <li>Giảm giá 10% phí thuê ấn phẩm khi đăng ký thẻ thành viên</li>
-                            </ul>
-                        </div>
-                    </div>
-                    <div class="col-lg-4 col-md-4">
-                        <div class="header__top__right">
-                            <div class="header__top__right__social">
-                                <a href="#"><i class="fa fa-user"></i> Đăng ký</a>
-                            </div>
-                            <div class="header__top__right__auth">
-                                <a href="#"><i class="fa fa-user"></i> Đăng nhập</a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-3">
-                    <div class="header__logo">
-                        <a href="./index.php"><img src="img/logo.png" alt=""></a>
-                    </div>
-                </div>
-                <div class="col-lg-7">
-                    <nav class="header__menu">
-                        <ul>
-                            <li class="active"><a href="./index.php">Trang chủ</a></li>
-                            <li><a href="./shop-grid.php">Sản phẩm</a></li>
-                            <li><a href="#">Giỏ hàng</a>
-                                <ul class="header__menu__dropdown">
-                                    <li><a href="./shop-details.html">Shop Details</a></li>
-                                    <li><a href="./shoping-cart.php">Shoping Cart</a></li>
-                                    <li><a href="./checkout.html">Check Out</a></li>
-                                </ul>
-                            </li>
-                            <li><a href="./contact.html">Liên hệ</a></li>
-                            <li><a href="./contact.html">Chính sách</a></li>
-
-                        </ul>
-                    </nav>
-                </div>
-                <div class="col-lg-2">
-                    <div class="header__cart">
-                        <div class="header__cart__price">Giỏ hàng: <span>$150.00</span></div>
-                    </div>
-                </div>
-            </div>
-            <div class="humberger__open">
-                <i class="fa fa-bars"></i>
-            </div>
-        </div>
-    </header>
-    <!-- Header Section End -->
-
-    <!-- Hero Section Begin -->
-    <section class="hero">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-3">
-                    <div class="hero__categories">
-                        <div class="hero__categories__all" style="background-color: #E75480;">
-                            <i class="fa fa-bars"></i>
-                            <span>DANH MỤC</span>
-                        </div>
-                        <div class="active">
-                            <ul>
-                                <li><a href="#">Sách Tiếng Việt</a></li>
-                                <li><a href="#">Sách Tiếng Anh</a></li>
-                                <li><a href="#">Tạp Chí Tiếng Anh</a></li>
-                                <li><a href="#">Tạp Chí Tiếng Việt</a></li>
-                                <li><a href="#">Truyện Tiếng Việt</a></li>
-                                <li><a href="#">Truyện Tiếng Anh</a></li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-9">
-                    <div class="hero__search">
-                        <div class="hero__search__form">
-                            <form action="#">
-                                <input type="text" placeholder="Bạn cần gì ?">
-                                <button type="submit" class="site-btn">TÌM KIẾM</button>
-                            </form>
-                        </div>
-                        <div class="hero__search__phone">
-                            <div class="hero__search__phone__text">
-                                <h5>+84 000.000.0</h5>
-                                <span>Hỗ trợ 24/7</span>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="hero__item" style="background-image: url('img/hero/banner.png');">
-                        <div class="hero__text" style="margin-left: 200px; text-align: center;">
-                            <h3 style="color: white; background-color: rgba(56, 49, 56, 0.156); width: 200px; margin-left: 50px; font-family:'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif">NEXUS</h3>
-                            <h2>Thuê là có <br/>Không đắn đo</h2>
-                            <a href="#" class="site-btn" style="margin-top: 15px;">SHOP NOW</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
-    <!-- Hero Section End -->
 
     <!-- Categories Section Begin -->
     <section class="categories">
@@ -162,7 +50,7 @@
     <!-- Categories Section End -->
 
     <!-- Sản phẩm được thuê nhiều -->
-    <section class="featured spad"> 
+    <section class="featured spad">
         <div class="container">
             <div class="row">
                 <div class="col-lg-12">
@@ -509,9 +397,9 @@
         </div>
     </section>
     <!-- Latest Product Section End -->
-    
+
     <!-- Footer -->
-    <?php require_once 'layout/footer.php'?>
+    <?php require_once 'layout/footer.php' ?>
 </body>
 
 </html>
--- FE/layout/header_section.php
+++ FE/layout/header_section.php
@@ -1,106 +1,38 @@
-<!-- Header Section Begin -->
-<header class="header">
-    <div class="header__top">
+<!-- Hero Section Begin-->
+<section class="hero">
         <div class="container">
             <div class="row">
-                <div class="col-lg-8 col-md-8">
-                    <div class="header__top__left">
-                        <ul>
-                            <li><i class="fa fa-envelope"></i>codenexus@gmail.com</li>
-                            <li>Giảm giá 10% phí thuê ấn phẩm khi đăng ký thẻ thành viên</li>
-                        </ul>
-                    </div>
-                </div>
-                <div class="col-lg-4 col-md-4">
-                    <div class="header__top__right">
-                        <div class="header__top__right__social">
-                            <a href="#"><i class="fa fa-user"></i> Đăng ký</a>
+                <div class="col-lg-3">
+                    <div class="hero__categories">
+                        <div class="hero__categories__all" style="background-color: #E75480;">
+                            <i class="fa fa-bars"></i>
+                            <span>DANH MỤC</span>
                         </div>
-                        <div class="header__top__right__auth">
-                            <a href="#"><i class="fa fa-user"></i> Đăng nhập</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-3">
-                <div class="header__logo">
-                    <a href="./index.php"><img src="img/logo.png" alt=""></a>
-                </div>
-            </div>
-            <div class="col-lg-7">
-                <nav class="header__menu">
-                    <ul>
-                        <li><a href="./index.php">Trang chủ</a></li>
-                        <li class="active"><a href="./shop-grid.php">Sản phẩm</a></li>
-                        <li><a href="#">Giỏ hàng</a>
-                            <ul class="header__menu__dropdown">
-                                <li><a href="./shop-details.php">Shop Details</a></li>
-                                <li><a href="./shoping-cart.php">Shoping Cart</a></li>
-                                <li><a href="./checkout.php">Check Out</a></li>
+                        <div class="active">
+                            <ul>
+                                <li><a href="#">Sách Tiếng Việt</a></li>
+                                <li><a href="#">Sách Tiếng Anh</a></li>
+                                <li><a href="#">Tạp Chí Tiếng Anh</a></li>
+                                <li><a href="#">Tạp Chí Tiếng Việt</a></li>
+                                <li><a href="#">Truyện Tiếng Việt</a></li>
+                                <li><a href="#">Truyện Tiếng Anh</a></li>
                             </ul>
-                        </li>
-                        <li><a href="./contact.php">Liên hệ</a></li>
-                        <li><a href="./contact.php">Chính sách</a></li>
-
-                    </ul>
-                </nav>
-            </div>
-            <div class="col-lg-2">
-                <div class="header__cart">
-                    <div class="header__cart__price">Giỏ hàng: <span>$150.00</span></div>
-                </div>
-            </div>
-        </div>
-        <div class="humberger__open">
-            <i class="fa fa-bars"></i>
-        </div>
-    </div>
-</header>
-<!-- Header Section End -->
-
-<!-- Hero Section Begin -->
-<section class="hero">
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-3">
-                <div class="hero__categories">
-                    <div class="hero__categories__all" style="background-color: #E75480;">
-                        <i class="fa fa-bars"></i>
-                        <span>DANH MỤC</span>
-                    </div>
-                    <div class="category-list">
-                        <ul>
-                            <li><a href="#">Sách Tiếng Việt</a></li>
-                            <li><a href="#">Sách Tiếng Anh</a></li>
-                            <li><a href="#">Tạp Chí Tiếng Anh</a></li>
-                            <li><a href="#">Tạp Chí Tiếng Việt</a></li>
-                            <li><a href="#">Truyện Tiếng Việt</a></li>
-                            <li><a href="#">Truyện Tiếng Anh</a></li>
-                        </ul>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div class="col-lg-9">
-                <div class="hero__search">
-                    <div class="hero__search__form">
-                        <form action="#">
-                            <input type="text" placeholder="Bạn cần gì ?">
-                            <button type="submit" class="site-btn">TÌM KIẾM</button>
-                        </form>
-                    </div>
-                    <div class="hero__search__phone">
-                        <div class="hero__search__phone__text">
-                            <h5>+84 000.000.0</h5>
-                            <span>Hỗ trợ 24/7</span>
+                <div class="col-lg-9">
+                    
+                    <div class="hero__item" style="background-image: url('img/hero/banner.png');">
+                        <div class="hero__text" style="margin-left: 200px; text-align: center;">
+                            <h3
+                                style="color: white; background-color: rgba(56, 49, 56, 0.156); width: 200px; margin-left: 50px; font-family:'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif">
+                                NEXUS</h3>
+                            <h2>Thuê là có <br />Không đắn đo</h2>
+                            <a href="#" class="site-btn" style="margin-top: 15px;">SHOP NOW</a>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-</section>
-<!-- Hero Section End -->
+    </section> 
+    <!-- Hero Section End -->
